7df1617a900b39939d29b5964454cabedcbb87e9
Dennis O'Loane <oloanedennis0@gmail.com>
2026-09-08 00:14:42 -0400

    Register Lambda scanner module

--- blacklight_security/scanners/aws/__init__.py
+++ blacklight_security/scanners/aws/__init__.py
@@ -3,7 +3,15 @@
 from .cloudtrail import CloudTrailScanner
 from .ec2 import EC2Scanner
 from .iam import IAMScanner
+from .lambda_functions import LambdaScanner
 from .rds import RDSScanner
 from .s3 import S3Scanner
 
-__all__ = ["CloudTrailScanner", "EC2Scanner", "IAMScanner", "RDSScanner", "S3Scanner"]
+__all__ = [
+    "CloudTrailScanner",
+    "EC2Scanner",
+    "IAMScanner",
+    "LambdaScanner",
+    "RDSScanner",
+    "S3Scanner",
+]
